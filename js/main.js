a19a9e3fbc28aaa1ee5bda811727e1c163cf6e29
Miano <dev.miano@gmail.com>
2022-03-06 18:36:28 +0300

    added the array for akan names

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 function akanForm() {
   var gender = document.getElementById('gender').value;
   var birthDay = document.getElementById('birthday').value;
+
+  // array of birth names according to the day
+  const akanArray = [
+    ['Sunday', 'Kwasi', 'Akosua'],
+    ['Monday', 'Kwado', 'Adwoa'],
+    ['Tuesday', 'Kwabena', 'Abenaa'],
+    ['Wednesday', 'Kwaku', 'Akua'],
+    ['Thursday', 'Yaw', 'Yaa'],
+    ['Friday', 'Kofi', 'Afua'],
+    ['Saturday', 'Kwame', 'Ama'],
+  ];
 }
